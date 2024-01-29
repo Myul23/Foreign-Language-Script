214ef5a33478ff83d0d8c3054935c5b206124e96
Myul23 <Myul23@users.noreply.github.com>
2024-01-29 10:20:10 +0900

    once

---
+++ README.md
@@ -0,0 +1,6 @@
+> ## Foreign Language Script
+
+
+- `file_read(address: str)` -> input_texts
+- input_texts -> `transform(input_texts: str)` -> output_texts
+- output_texts -> `file_save(texts: str, address: str)`
---
+++ foreign_language_script.py
@@ -0,0 +1,42 @@
+import os
+
+
+def file_read(address: str):
+    if not os.path.exists(address):
+        print(address, "file doesn't exist")
+        raise FileNotFoundError
+
+    with open(address, "r") as tf:
+        texts = tf.readlines()
+    return texts
+
+
+def transform(input_texts: str):
+    output_texts, text = [], []
+    for line in input_texts:
+        line = line.rstrip("\n")
+        if len(line) < 1:
+            continue
+
+        if ":" in line[:6]:
+            output_texts.append(" ".join(text))
+            text = []
+
+            line = "`" + line + "`"
+        text.append(line)
+    return "## " + "\n\n<br>\n\n".join(output_texts)
+
+
+def file_save(texts: str, address: str):
+    if os.path.exists(address):
+        print(address, "file already existed")
+        raise FileExistsError
+
+    with open(address, "w") as tf:
+        tf.writelines(texts)
+
+
+if __name__ == "__main__":
+    script_input = "Foreign Language Script/foreign_language_script_test_input.txt"
+    script_output = "Foreign Language Script/foreign_language_script_test_output.txt"
+    file_save(transform(file_read(script_input)), script_output)
